ec11a8f1892e83628bc4ec40886cac96ca6b11aa
chrispywtr <chris@susxwebs.com>
2022-07-12 14:35:29 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-angular';
+  newDriverName = "";
+  drivers: string[] = [];
+  errorMessage = '';
+  numberOfTeams: number | "" = "";
+  teams: string[][] = [];
+
+  onInput(driver: string) {
+    this.newDriverName = driver
+  }
+  addDriver() {
+    if(!this.newDriverName) {
+      this.errorMessage = "Name can't be empty"
+      return
+    } 
+    this.drivers.push(this.newDriverName)
+    console.log(this.drivers)
+    this.newDriverName = ""
+    this.errorMessage = ""
+  }
+
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value)
+  }
+
+  generateTeams() {
+    if(!this.numberOfTeams || this.numberOfTeams <= 0){
+      this.errorMessage = "Invalid number of teams"
+      return
+    }
+    if(this.drivers.length < this.numberOfTeams){
+      this.errorMessage = "Not enough drivers for amount of teams"
+      return
+    }
+    const allDrivers = [...this.drivers]
+    while(allDrivers.length){
+      for(let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allDrivers.length)
+        const driver = allDrivers.splice(randomIndex, 1)[0];
+        if(!driver)break;
+        if (this.teams[i]) {
+         this.teams[i].push(driver)
+        } else {
+         this.teams[i] = [driver]
+        }
+     }
+    }
+    console.log(this.teams)
+    this.drivers = []
+    this.numberOfTeams = ""
+  }
 }
